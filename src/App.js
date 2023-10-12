85dddfd0aabcd4b3dc12bd1fb4bf9c959b180eb1
Manaobhi Sri Ram <manobhisriram@gmail.com>
2023-10-12 17:37:43 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,31 +1,32 @@
 
-import React, { useState } from 'react';
-import './App.css';
-import InputField from './components/InputField';
-import CalculateButton from './components/CalculateButton';
-import AgeDisplay from './components/AgeDisplay';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [birthYear, setBirthYear] = useState('');
-  const [age, setAge] = useState('');
+  const [dob, setDob] = useState("");
+  const [age, setAge] = useState("");
 
   const calculateAge = () => {
-    const currentYear = new Date().getFullYear();
-    const enteredYear = parseInt(birthYear);
-    if (!isNaN(enteredYear) && enteredYear <= currentYear) {
-      const calculatedAge = currentYear - enteredYear;
-      setAge(`Your age is ${calculatedAge} years.`);
-    } else {
-      setAge('Please enter a valid birth year.');
-    }
+    const dobDate = new Date(dob);
+    const today = new Date();
+    const ageDiff = today - dobDate;
+    const calculatedAge = new Date(ageDiff);
+    setAge(Math.abs(calculatedAge.getUTCFullYear() - 1970));
   };
 
   return (
     <div className="App">
       <h1>Age Calculator</h1>
-      <InputField value={birthYear} onChange={(e) => setBirthYear(e.target.value)} />
-      <CalculateButton onClick={calculateAge} />
-      <AgeDisplay age={age} />
+      <div>
+        <label>Enter your date of birth: </label>
+        <input
+          type="date"
+          value={dob}
+          onChange={(e) => setDob(e.target.value)}
+        />
+        <button onClick={calculateAge}>Calculate Age</button>
+      </div>
+      {age && <h2>Your age is: {age} years</h2>}
     </div>
   );
 }
